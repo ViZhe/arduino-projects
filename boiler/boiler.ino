0d4e3ba95f97eb2e9b844d39f3c2c91da956d6e3
ViZhe <vizhe.cat@ya.ru>
2016-09-05 11:16:39 +0300

    add joystick && can change time & accuracy

--- boiler/boiler.ino
+++ boiler/boiler.ino
@@ -41,7 +41,7 @@ int pinRelay = 8;
 int pinAxisY = 0;
 int pinAxisX = 1;
 int pinAxisZ = 7;
-int value_X, value_Y, value_Z = 0;
+int joystickX, joystickY, joystickZ = 0;
 
 String releController;
 
@@ -52,10 +52,14 @@ int accuracyPlace = 0;
 int accuracy = 15;
 int TimeZoneAccuracy, TimeZoneAccuracyInvert;
 
+bool backlight = false;
+int backlightTimeout = 0;
+
+int editTimeMode = 0;
+
 void setup() {
   Serial.begin(9600);
   LCD.init();
-  LCD.backlight();
   RTC.Begin();
 
   pinMode(pinLedGreen, OUTPUT);
@@ -72,14 +76,7 @@ void setup() {
   TimeZoneAccuracy = RTC.GetMemory(accuracyPlace);
   if (!TimeZoneAccuracy) {
     RTC.SetMemory(0, accuracy);
-    TimeZoneAccuracy = accuracy;
   }
-
-  TimeZoneAccuracyInvert = 60 - TimeZoneAccuracy;
-
-  // if you are using ESP-01 then uncomment the line below to reset the pins to
-  // the available pins for SDA, SCL
-  // Wire.begin(0, 2); // due to limited pins, use pin 0 and 2 for SDA, SCL //TODO: TEST IT
 
   RtcDateTime compiled = RtcDateTime(__DATE__, __TIME__);
   Serial.print("compiled: ");
@@ -96,44 +93,54 @@ void setup() {
   }
 
   RtcDateTime now = RTC.GetDateTime();
-
   if (now < compiled) {
     Serial.println("RTC is older than compile time!  (Updating DateTime)");
     RTC.SetDateTime(compiled);
-  } else if (now > compiled) {
-    Serial.println("RTC is newer than compile time. (this is expected)");
-  } else if (now == compiled) {
-    Serial.println("RTC is the same as compile time! (not expected but all is fine)");
   }
 
-  RTC.SetSquareWavePin(DS1307SquareWaveOut_Low);
+  // RTC.SetSquareWavePin(DS1307SquareWaveOut_High);
+
+  LCD.setCursor(0, 2);
+  LCD.print("3OHA  +- TEMP  PEJIE");
 }
 
 void loop() {
-  value_X = analogRead(pinAxisX);    // Считываем аналоговое значение оси Х
-  Serial.print("X:");
-  Serial.print(value_X);      // Выводим значение в Serial Monitor
+  TimeZoneAccuracy = RTC.GetMemory(accuracyPlace);
+  TimeZoneAccuracyInvert = 60 - TimeZoneAccuracy;
+  joystickX = analogRead(pinAxisX);
+  joystickY = analogRead(pinAxisY);
+  joystickZ = digitalRead(pinAxisZ) ^ 1;
+  // value_Z = value_Z ^ 1;
+  //
+  LCD.backlight();
+  // if (!backlight && value_Z) {
+  //   backlightTimeout = 10;
+  //   LCD.backlight();
+  //   backlight = true;
+  //   Serial.print(1);
+  // } else if (backlightTimeout > 0) {
+  //   backlightTimeout = backlightTimeout - 1;
+  //   Serial.print(2);
+  // } else if (backlight) {
+  //   // LCD.noBacklight();
+  //   backlight = false;
+  //   Serial.print(3);
+  // }
+  //
+  // value_X = analogRead(pinAxisX);    // Считываем аналоговое значение оси Х
+  // Serial.print("X:");
+  // Serial.print(value_X);      // Выводим значение в Serial Monitor
+  //
+  // value_Y = analogRead(pinAxisY);    // Считываем аналоговое значение оси Y
+  // Serial.print(" | Y:");
+  // Serial.print(value_Y, DEC);      // Выводим значение в Serial Monitor
+  //
+  // // value_Z = digitalRead(pinAxisZ);   // Считываем цифровое значение оси Z (кнопка)
+  // // value_Z = value_Z ^ 1; // инвертируем значение
+  // Serial.print(" | Z: ");
+  // Serial.println(value_Z);
 
-  value_Y = analogRead(pinAxisY);    // Считываем аналоговое значение оси Y
-  Serial.print(" | Y:");
-  Serial.print(value_Y, DEC);      // Выводим значение в Serial Monitor
-
-  value_Z = digitalRead(pinAxisZ);   // Считываем цифровое значение оси Z (кнопка)
-  value_Z = value_Z ^ 1; // инвертируем значение
-  Serial.print(" | Z: ");
-  Serial.println(value_Z);
-
-  LCD.setCursor(15, 3);
-  if (!digitalRead(tumblerOn)) {
-    releController = "on";
-    LCD.print("*");
-  } else if (!digitalRead(tumblerOff)) {
-    releController = "off";
-    LCD.print("*");
-  } else {
-    releController = "auto";
-    LCD.print(" ");
-  }
+  releController = getReleControllerValue();
 
   if (!RTC.IsDateTimeValid()) {
     Serial.println("RTC lost confidence in the DateTime!");
@@ -145,22 +152,20 @@ void loop() {
   LCD.print(getDateTime(now));
   LCD.setCursor(18, 0);
   switch (now.DayOfWeek()) {
+    case 0: LCD.print("BC"); break;
     case 1: LCD.print("nH"); break;
     case 2: LCD.print("BT"); break;
     case 3: LCD.print("CP"); break;
     case 4: LCD.print("4T"); break;
     case 5: LCD.print("nT"); break;
     case 6: LCD.print("C6"); break;
-    case 7: LCD.print("BC"); break;
   }
-  Serial.println(getDateTime(now)); // TODO: only for test
+  // Serial.println(getDateTime(now)); // TODO: only for test
 
   temp = DS18B20(tempAddr);
 
-  LCD.setCursor(0, 2);
-  LCD.print("3OHA  +- TEMP  PEJIE");
   LCD.setCursor(6, 3);
-  LCD.print(TimeZoneAccuracy);
+  LCD.print(RTC.GetMemory(accuracyPlace));
   LCD.setCursor(9, 3);
   LCD.print(temp, 1);
 
@@ -182,8 +187,19 @@ void loop() {
     // 23:15 - 6:44
     doControl(3);
   }
-  delay (1000);
+
+  delay(100);
+
+  joystickControl();
+
+  delay(100);
 }
+
+/*
+ *
+ * FUNCTIONS
+ *
+ */
 
 #define countof(a) (sizeof(a) / sizeof(a[0]))
 
@@ -194,9 +210,9 @@ String getDateTime(const RtcDateTime& dt) {
     datestring,
     countof(datestring),
     PSTR("%02u/%02u/%02u %02u:%02u:%02u"),
-    dt.Day(),
-    dt.Month(),
     dt.Year()-2000,
+    dt.Month(),
+    dt.Day(),
     dt.Hour(),
     dt.Minute(),
     dt.Second()
@@ -219,64 +235,81 @@ String formatTime(int hour, int minute) {
   return timestring;
 }
 
-void doControl(int mode) {
+String getReleControllerValue() {
+  LCD.setCursor(15, 3);
+
+  if (!digitalRead(tumblerOn)) {
+    LCD.print("*");
+    return "on";
+  }
+  if (!digitalRead(tumblerOff)) {
+    LCD.print("*");
+    return "off";
+  }
+
+  LCD.print(" ");
+  return "auto";
+}
+
+void doControlLed(String mode, bool red, bool yellow, bool green) {
   LCD.setCursor(0, 3);
+  LCD.print(mode);
+  digitalWrite(pinLedRed, red);
+  digitalWrite(pinLedYellow, yellow);
+  digitalWrite(pinLedGreen, green);
+}
+
+void doControl(int mode) {
+
+  LCD.setCursor(17, 3);
+  if (releController == "on") {
+    LCD.print(" ON");
+    digitalWrite(pinRelay, LOW);
+  } else if (releController == "off") {
+    LCD.print("OFF");
+    digitalWrite(pinRelay, HIGH);
+  }
+
   switch (mode) {
     case 1:
-      LCD.print("$$$");
-      digitalWrite(pinLedRed, HIGH);
-      digitalWrite(pinLedYellow, LOW);
-      digitalWrite(pinLedGreen, LOW);
+      doControlLed("$$$", true, false, false);
 
-      LCD.setCursor(17, 3);
-      if (releController == "on") {
-        LCD.print(" ON");
-        digitalWrite(pinRelay, LOW);
-      } else {
+      if (releController == "auto") {
+        LCD.setCursor(17, 3);
         LCD.print("OFF");
         digitalWrite(pinRelay, HIGH);
       }
       break;
+
     case 2:
-      LCD.print("$$ ");
-      digitalWrite(pinLedRed, LOW);
-      digitalWrite(pinLedYellow, HIGH);
-      digitalWrite(pinLedGreen, LOW);
+      doControlLed("$$ ", false, true, false);
 
-      LCD.setCursor(17, 3);
-      if (releController == "on") {
-        LCD.print(" ON");
-        digitalWrite(pinRelay, LOW);
-      } else if (releController == "off") {
-        LCD.print("OFF");
-        digitalWrite(pinRelay, HIGH);
-      } else if (temp < 40) {
-        LCD.print(" ON");
-        digitalWrite(pinRelay, LOW);
-      } else {
-        LCD.print("OFF");
-        digitalWrite(pinRelay, HIGH);
+      if (releController == "auto") {
+        LCD.setCursor(17, 3);
+
+        if (temp < 40) {
+          LCD.print(" ON");
+          digitalWrite(pinRelay, LOW);
+        } else {
+          LCD.print("OFF");
+          digitalWrite(pinRelay, HIGH);
+        }
       }
       break;
-    default:
-      LCD.print("$  ");
-      digitalWrite(pinLedRed, LOW);
-      digitalWrite(pinLedYellow, LOW);
-      digitalWrite(pinLedGreen, HIGH);
 
-      LCD.setCursor(17, 3);
-      if (releController == "on") {
-        LCD.print(" ON");
-        digitalWrite(pinRelay, LOW);
-      } else if (releController == "off") {
-        LCD.print("OFF");
-        digitalWrite(pinRelay, HIGH);
-      } else if (temp < 80) {
-        LCD.print(" ON");
-        digitalWrite(pinRelay, LOW);
-      } else {
-        LCD.print("OFF");
-        digitalWrite(pinRelay, HIGH);
+    default:
+      doControlLed("$  ", false, false, true);
+
+      if (releController == "auto") {
+        LCD.setCursor(17, 3);
+
+        if (temp < 80) {
+          LCD.print(" ON");
+          digitalWrite(pinRelay, LOW);
+        } else {
+          LCD.print("OFF");
+          digitalWrite(pinRelay, HIGH);
+        }
       }
   }
 }
@@ -295,4 +328,82 @@ float DS18B20(byte *adres) {
   raw = (data[1] << 8) | data[0];
   float celsius = (float)raw / 16.0;
   return celsius;
+}
+
+
+void joystickControl() {
+  int pressDelay = 0;
+
+  if (editTimeMode) {
+    if (editTimeMode == 7) {
+      LCD.setCursor(6, 3);
+    } else {
+      LCD.setCursor(15 - ((editTimeMode - 1) * 3), 0);
+    }
+    LCD.print("  ");
+
+    if (joystickY >= 600) {
+      RtcDateTime now = RTC.GetDateTime();
+
+      switch (editTimeMode) {
+        case 1: RTC.SetDateTime(now + 1); break;
+        case 2: RTC.SetDateTime(now + 60); break;
+        case 3: RTC.SetDateTime(now + 3600); break;
+        case 4: RTC.SetDateTime(now + 86400); break;
+        case 5: RTC.SetDateTime(now + 2592000); break;
+        case 6: RTC.SetDateTime(now + 31104000); break;
+        case 7:
+          int accuracyOld = RTC.GetMemory(accuracyPlace);
+          int accuracyNew = accuracyOld == 15 ? 15 : accuracyOld + 1;
+          RTC.SetMemory(0, accuracyNew);
+          LCD.setCursor(6, 3);
+          LCD.print(accuracyNew);
+          break;
+      }
+      RTC.SetIsRunning(false);
+      RTC.SetIsRunning(true);
+
+    }
+    if (joystickY <= 400) {
+      RtcDateTime now = RTC.GetDateTime();
+
+      switch (editTimeMode) {
+        case 1: RTC.SetDateTime(now - 1); break;
+        case 2: RTC.SetDateTime(now - 60); break;
+        case 3: RTC.SetDateTime(now - 3600); break;
+        case 4: RTC.SetDateTime(now - 86400); break;
+        case 5: RTC.SetDateTime(now - 2592000); break;
+        case 6: RTC.SetDateTime(now - 31104000); break;
+        case 7:
+          int accuracyOld = RTC.GetMemory(accuracyPlace);
+          int accuracyNew = accuracyOld == 0 ? 0 : accuracyOld - 1;
+          RTC.SetMemory(0, accuracyNew);
+          LCD.setCursor(6, 3);
+          LCD.print(accuracyNew);
+          break;
+      }
+      RTC.SetIsRunning(false);
+      RTC.SetIsRunning(true);
+    }
+
+    if (joystickX >= 600) {
+      editTimeMode = editTimeMode == 1 ? 7 : editTimeMode - 1;
+    }
+    if (joystickX <= 400) {
+      editTimeMode = editTimeMode == 7 ? 1 : editTimeMode + 1;
+    }
+
+    if (joystickZ) {
+      editTimeMode = 0;
+    }
+  } else {
+    if (joystickZ) {
+      while(digitalRead(pinAxisZ) ^ 1) {
+        pressDelay++;
+      }
+      if (pressDelay > 10) {
+        editTimeMode = 1;
+      }
+    }
+  }
 }
